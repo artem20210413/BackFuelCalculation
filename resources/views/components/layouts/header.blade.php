818ef690831d2f319872b4efd736b26bdd70edb8
Artem Tishchenko <82469944+artem20210413@users.noreply.github.com>
2024-02-25 16:09:35 +0200

    chacnge button

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,7 +1,9 @@
 <header class="d-flex justify-content-between p-3 shadow">
     <div>
         <button type="button" class="btn btn-primary"
-                onclick=" window.location.href = '{{ \Illuminate\Support\Facades\Redirect::back()->getTargetUrl() }}'">Назд
+                {{--                onclick=" window.location.href = '{{ \Illuminate\Support\Facades\Redirect::back()->getTargetUrl() }}'">Назд--}}
+                onclick=" window.location.href = '{{ \Illuminate\Support\Facades\Redirect::route('fuel-statistic-list')->getTargetUrl() }}'">
+            Статистика палива
         </button>
     </div>
     <div>
